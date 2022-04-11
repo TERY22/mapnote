edc3e004e25ce3cd9986b40301bb68eddaacd2d4
TERY <fire_jacky@mail.ru>
2022-04-11 14:12:45 +0300

    homework-8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { LatLng, TileLayer } from 'leaflet';
-import { LeafletMap } from './lib';
+import { AfterViewInit, Component } from "@angular/core"
+import { LatLng, TileLayer } from "leaflet"
+import { LeafletMap } from "./lib"
 
 @Component({
-  selector: 'mn-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "mn-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [ "./app.component.scss" ]
 })
 export class AppComponent implements AfterViewInit {
   public ngAfterViewInit(): void {
-    const map = new LeafletMap('map-container', {
+    const map = new LeafletMap("map-container", {
       layers: [
-        new TileLayer('', {
-          subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-        }) 
+        new TileLayer("https://{s}.google.com/vt?x={x}&y={y}&z={z}", {
+          subdomains: [ 'mt0', 'mt1', 'mt2', 'mt3' ]
+        })
       ],
-      center: new LatLng(55.7522, 37.6156),
+      center: new LatLng(45.040034, 38.975828),
       zoom: 13
     })
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
 
-import { AppComponent } from './app.component';
-import { ShellModule } from './ui/shell/components/shell.module';
+import { AppComponent } from "./app.component"
+import { PlaceEditDialogModule } from "./ui/place-edit-dialog/place-edit-dialog.module"
+import { ShellModule } from "./ui/shell/shell.module"
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ShellModule
+    ShellModule,
+    PlaceEditDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
